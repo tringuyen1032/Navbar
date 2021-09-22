67138ff8cedd0b1fffd0355752b3a49e80f82320
tringuyen1032 <trinmse140981@fpt.edu.vn>
2021-09-22 23:49:23 +0700

    hello everyone

--- src/Components/Home/Navigation/NavigationBar.js
+++ src/Components/Home/Navigation/NavigationBar.js
@@ -15,89 +15,91 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 
 export default function NavigationBar() {
-  const ariaLabel = { 'aria-label': 'description' };
-  const menuId = 'primary-search-account-menu';
-  const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [showNav, setShowNav] = useState(true);
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const changeToSearchBar = () => {
-    setShowNav(!showNav);
-  }
-  const navBar =
-    (<Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-      <CustomizedMenus name="Home" />
-      <CustomizedMenus name="Us Model" />
-      <CustomizedMenus name="Our World" />
-      <CustomizedMenus name="Be creative" />
-      <IconButton
-        size="large"
-        aria-label="show 17 new notifications"
-        color="inherit"
-      >
-        <Badge badgeContent={17} color="error">
-          <NotificationsIcon />
-        </Badge>
-      </IconButton>
-      <IconButton
-        size="large"  
-        aria-label="account of current user"
-        aria-controls={menuId}
-        aria-haspopup="true"
-        onClick={handleProfileMenuOpen}
-        color="inherit"
-      >
-        <AccountCircleIcon/>
-      </IconButton>
-      <IconButton
-        size="large"
-        aria-label="account of current user"
-        aria-controls={menuId}
-        aria-haspopup="true"
-        onClick={handleProfileMenuOpen}
-        color="inherit"
-      >
-        <SearchIcon onClick={() => changeToSearchBar()}/> 
-      </IconButton>
-    </Box>)
+   // const ariaLabel = { 'aria-label': 'description' };
+   // const menuId = 'primary-search-account-menu';
+   // const classes = useStyles();
+   // const [setAnchorEl] = React.useState(null);
+   // const [showNav, setShowNav] = useState(true);
+   // const handleProfileMenuOpen = (event) => {
+   //    setAnchorEl(event.currentTarget);
+   // };
+   // const changeToSearchBar = () => {
+   //    setShowNav(!showNav);
+   // }
+   const navBar =
+      (<Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+         <CustomizedMenus name="Home" />
+         <CustomizedMenus name="Us Model" />
+         <CustomizedMenus name="Our World" />
+         <CustomizedMenus name="Be creative" />
+         <IconButton
+            size="large"
+            aria-label="show 17 new notifications"
+            color="inherit"
+         >
+            <Badge badgeContent={17} color="error">
+               <NotificationsIcon />
+            </Badge>
+         </IconButton>
+         <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls='primary-search-account-menu'
+            aria-haspopup="true"
+            onClick={handleProfileMenuOpen}
+            color="inherit"
+         >
+            <AccountCircleIcon />
+         </IconButton>
+         <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls='primary-search-account-menu'
+            aria-haspopup="true"
+            onClick={handleProfileMenuOpen}
+            color="inherit"
+         >
+            <SearchIcon onClick={() => changeToSearchBar()} />
+         </IconButton>
+      </Box>)
 
-  const searchBar = 
-  (<Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-    <Input placeholder="Enter to search" inputProps={ariaLabel} disableUnderline="false" className={classes.searchInput_Style} />
-    <IconButton
-      size="large"
-      edge="end"
-      aria-label="account of current user"
-      aria-controls={menuId}
-      aria-haspopup="true"
-      onClick={handleProfileMenuOpen}
-      color="inherit"
-    >
-      <CloseIcon onClick={() => changeToSearchBar()} />
-    </IconButton>
-  </Box>) 
-  
-  let bar = null;
-  showNav ? (bar = navBar) : (bar = searchBar)
+   const searchBar =
+      (<Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+         <Input placeholder="Enter to search" inputProps={ariaLabel} disableUnderline="false" className={classes.searchInput_Style} />
+         <IconButton
+            size="large"
+            edge="end"
+            aria-label="account of current user"
+            aria-controls='primary-search-account-menu'
+            aria-haspopup="true"
+            onClick={handleProfileMenuOpen}
+            color="inherit"
+         >
+            <CloseIcon onClick={() => changeToSearchBar()} />
+         </IconButton>
+      </Box>)
 
-  return (
-    <AppBar className={classes.appBar_style} >
-      <Toolbar className={classes.toolBar_style} >
-        <Typography
-          variant="h6"
-          noWrap
-          component="div"
-          sx={{ display: { xs: 'none', sm: 'block' } }}
-        >
-          <span className={classes.appName_style}>PIMO</span>
-        </Typography>
-        <Box sx={{ flexGrow: 0.8 }} />
-        {bar}
-      </Toolbar>
-    </AppBar>
-  );
+   let bar = null;
+   showNav ? (bar = navBar) : (bar = searchBar)
+
+   return (
+      <>
+      <AppBar className={classes.appBar_style} >
+         <Toolbar className={classes.toolBar_style} >
+            <Typography
+               variant="h6"
+               noWrap
+               component="div"
+               sx={{ display: { xs: 'none', sm: 'block' } }}
+            >
+               <span className={classes.appName_style}>PIMO</span>
+            </Typography>
+            <Box sx={{ flexGrow: 0.8 }} />
+            {bar}
+         </Toolbar>
+      </AppBar>
+      </>
+   );
 
 }
 
--- src/Components/Home/Navigation/Toggle.js
+++ src/Components/Home/Navigation/Toggle.js
@@ -1,17 +1,89 @@
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import React from 'react';
-
+import style from './vi.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Badge from '@mui/material/Badge';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import CustomizedMenus from './menu';
+import CloseIcon from '@mui/icons-material/Close';
+import Input from '@mui/material/Input';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 class Toggle extends React.Component {
-  render() {
-        return <div>
-          <ReactCSSTransitionGroup
-          transitionName="toggle"
-          transitionEnterTimeout={300}
-          transitionLeaveTimeout={300}>
+   render() {
+      return <div>
+         <ReactCSSTransitionGroup
+            transitionName="toggle"
+            transitionEnterTimeout={300}
+            transitionLeaveTimeout={300}>
             {this.props.hidden ? null : <div className="toggle-base">{this.props.children}</div>}
-          </ReactCSSTransitionGroup>    
-       </div>
-  }
+         </ReactCSSTransitionGroup>
+      </div>
+   }
 }
-export default Toggle;
+
+export default class Test extends React.Component {
+   constructor(props) {
+      super(props);
+      this.onClick = this.onClick.bind(this);
+      this.state = { hidden: true, show: true };
+   }
+
+   onClick() {
+      this.setState((prevState, props) => ({
+         hidden: !(prevState.hidden),
+         show: !(prevState.show)
+      }));
+   }
+   render() {
+      return <div className="navbar">
+         <div className="navbar_inner">
+            <Toggle hidden={this.state.hidden}>
+               <Input placeholder="Enter to search" disableUnderline="false" className="searchInput_Style" />
+            </Toggle>
+            <Toggle hidden={!this.state.show}>
+               <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                  <CustomizedMenus name="Home" />
+                  <CustomizedMenus name="Us Model" />
+                  <CustomizedMenus name="Our World" />
+                  <CustomizedMenus name="Be creative" />
+                  <IconButton
+                     size="large"
+                     aria-label="show 17 new notifications"
+                     color="inherit"
+                  >
+                     <Badge badgeContent={17} color="error">
+                        <NotificationsIcon />
+                     </Badge>
+                  </IconButton>
+                  <IconButton
+                     size="large"
+                     aria-label="account of current user"
+                     aria-controls='primary-search-account-menu'
+                     aria-haspopup="true"
+                     color="inherit"
+                  >
+                     <AccountCircleIcon />
+                  </IconButton>
+               </Box>
+            </Toggle>
+         </div>
+         <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls='primary-search-account-menu'
+            aria-haspopup="true"
+            color="inherit"
+            onClick={this.onClick}
+         >
+            {this.state.hidden === true ?
+               (<SearchIcon />) : (<CloseIcon />)}
+         </IconButton>
+      </div>;
+   }
+}
 
